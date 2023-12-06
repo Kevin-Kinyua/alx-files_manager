ef39a378be510990df873cc04b6298ef944b3729
Kevin-Kinyua <w.kevinkinyua@gmail.com>
2023-12-06 01:49:15 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,30 @@
 #!/usr/bin/node
 
-const express = require("express");
-const router = require("./routes/index");
+import express from 'express';
+import router from './routes/index';
+import unmatchedRouteHandler from './middleware/unmatched';
+import errorHandler from './middleware/error';
+import shutdown from './utils/shutdown';
 
-const server = express();
-const PORT = process.env.PORT ? process.env.PORT : 5000;
+// Express server
+const app = express();
+const PORT = process.env.PORT || 5000;
 
-server.use(express.json());
-server.use(router);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(router);
+app.use(unmatchedRouteHandler);
+app.use(errorHandler);
 
-server.listen(PORT, () =>
-  console.log(`The server is running on port: ${PORT}`)
-);
+const server = app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Server running on port ${PORT}`);
+});
+
+// Graceful shutdown
+const handler = () => shutdown(server);
+process.on('SIGINT', handler);
+process.on('SIGTERM', handler);
+process.on('SIGQUIT', handler);
+
+export default app;
